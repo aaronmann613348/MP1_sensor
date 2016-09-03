190f1e59e732ea5cb4ffd25bc119ab91a4798dde
YifangZhang <yifangzhang2009@gmail.com>
2016-09-03 15:26:19 -0500

    Initial Commit

---
+++ MP1_Mobile_sensor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MP1_Mobile_sensor
+//
+//  Created by Yifang Zhang on 9/3/16.
+//  Copyright © 2016 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MP1_Mobile_sensorTests/MP1_Mobile_sensorTests.m
@@ -0,0 +1,39 @@
+//
+//  MP1_Mobile_sensorTests.m
+//  MP1_Mobile_sensorTests
+//
+//  Created by Yifang Zhang on 9/3/16.
+//  Copyright © 2016 Yifang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MP1_Mobile_sensorTests : XCTestCase
+
+@end
+
+@implementation MP1_Mobile_sensorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
